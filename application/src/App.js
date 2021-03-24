c1bcb0343412daba207471c15c0d06d3df389074
kikiyaki <krl9628@gmail.com>
2021-03-25 01:58:05 +0300

    routing

--- application/src/App.js
+++ application/src/App.js
@@ -1,49 +1,20 @@
 import React from 'react';
-import TaskList from "./Task/TaskList/TaskList";
-import Context from "./context";
-import AddTask from "./Task/AddTask";
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Logout from "./Pages/Logout"
 
 function App() {
-    const [taskList, setTaskList] = React.useState([
-        {id: 1, title: 'Training', ready: true},
-        {id: 3, title: 'Shopping', ready: false}
-    ]);
-
-    function readyOnChange(id) {
-        setTaskList(taskList.map(task => {
-                if (task.id === id) {
-                    task.ready = !task.ready
-                }
-
-                return task
-            })
-        );
-    }
-
-    function removeTask(id) {
-        setTaskList(taskList.filter(task => task.id !== id))
-    }
-
-    function onCreate(value) {
-        setTaskList(taskList.concat([{
-            id: Math.floor(Math.random() * 100),
-            title: value,
-            ready: false
-        }]))
-    }
-
   return (
-      <Context.Provider value={{removeTask}}>
+      <Router>
           <div className="wrapper">
-              <h1>Task List</h1>
-              <AddTask onCreate={onCreate}/>
-              {
-                  taskList.length
-                  ? <TaskList taskList={taskList} readyOnChange={readyOnChange}/>
-                  : 'No tasks'
-              }
+              <Route path="/" exact component={Home}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+              <Route path="/logout" component={Logout}/>
           </div>
-      </Context.Provider>
+      </Router>
   );
 }
 
---
+++ application/src/Pages/Home.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Context from "../context";
+import AddTask from "../Task/AddTask";
+import TaskList from "../Task/TaskList/TaskList";
+
+export default function Home() {
+    const [taskList, setTaskList] = React.useState([
+        {id: 1, title: 'Training', ready: true},
+        {id: 3, title: 'Shopping', ready: false}
+    ]);
+
+    function readyOnChange(id) {
+        setTaskList(taskList.map(task => {
+                if (task.id === id) {
+                    task.ready = !task.ready
+                }
+
+                return task
+            })
+        );
+    }
+
+    function removeTask(id) {
+        setTaskList(taskList.filter(task => task.id !== id))
+    }
+
+    function onCreate(value) {
+        setTaskList(taskList.concat([{
+            id: Math.floor(Math.random() * 100),
+            title: value,
+            ready: false
+        }]))
+    }
+
+    return (
+        <Context.Provider value={{removeTask}}>
+                <h1>Task List</h1>
+                <AddTask onCreate={onCreate}/>
+                {
+                    taskList.length
+                        ? <TaskList taskList={taskList} readyOnChange={readyOnChange}/>
+                        : 'No tasks'
+                }
+        </Context.Provider>
+    )
+}
