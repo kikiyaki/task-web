2f8e8a2aa608ead72bd1bb50cad41e666e1a718d
kikiyaki <krl9628@gmail.com>
2021-03-25 23:20:55 +0300

    auth nav

--- application/src/App.js
+++ application/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./app.css"
 import {Route, BrowserRouter as Router} from 'react-router-dom'
 import Home from "./Pages/Home"
 import Login from "./Pages/Login"
@@ -8,6 +9,12 @@ import Logout from "./Pages/Logout"
 function App() {
   return (
       <Router>
+          <div className="auth-box">
+              <a href="/">Home</a>
+              <a href="/login">Login</a>
+              <a href="/logout">Logout</a>
+              <a href="/register">Registration</a>
+          </div>
           <Route path="/" exact component={Home}/>
           <Route path="/login" component={Login}/>
           <Route path="/register" component={Register}/>
