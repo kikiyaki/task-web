44a293cc3168ee12aecf1656d95ea67f758a5fc0
kikiyaki <krl9628@gmail.com>
2021-03-26 01:35:13 +0300

    save csrf token and cookies

--- application/src/Pages/Login.js
+++ application/src/Pages/Login.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useCookies } from 'react-cookie'
 
 export default function Login() {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [cookies, setCookie] = useCookies(['name']);
+
+    function onSubmit(event) {
+        event.preventDefault()
+        fetch('http://localhost/token', {
+            credentials: 'include',
+        })
+            .then(data => data.json())
+            .then(data => {
+                setCookie('XSRF-TOKEN', data.data)
+            })
+    }
+
     return (
         <div className="wrapper">
             <h1>Login</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>
                     <p>Email</p>
                     <input/>
